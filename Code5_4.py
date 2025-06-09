0015aa8a2899527f22b3e336f54c4e1c3a21a0b5
yuan303 <yuan303@126.com>
2025-06-09 14:09:52 +0800

    New Try

---
+++ Code5_4.py
@@ -0,0 +1,21 @@
+str1 = 'I am learning Python 101!'
+print(str1)
+
+str2 =  "Python is fun .Machine learning is fun too."
+print(str2)
+
+str4 = 'Hey,' + 'James!'
+print(str4)
+
+str5 = 'Python is FUN!'
+str6 = str5 * 3
+print(str6)
+
+str7 = '123'
+str8 = str7 * 3
+print(str8)
+
+str9 = '456'
+str10 = str7 + str9
+print(str(10))
+print(type(str10))
