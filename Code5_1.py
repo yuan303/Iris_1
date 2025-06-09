9790103e2db820e64bfab05f713d38b90595ccea
yuan303 <yuan303@126.com>
2025-06-09 13:52:41 +0800

    New Try

---
+++ Code5_1.py
@@ -0,0 +1,11 @@
+x = 88
+y = -8.88
+z = 8 + 8j
+print(type(x))
+print(type(y))
+print(type(z))
+print("Hello")
+
+'''
+dssdf 
+'''
---
+++ Code5_2.py
@@ -0,0 +1,10 @@
+x = 88.8
+y = 8
+x_to_int = int(x)
+print(x_to_int)
+y_to_float =float(y)
+print(y_to_float)
+y_to_complex = complex(y)
+print(y_to_complex)
+x_to_str = str(x)
+print(x_to_str)
